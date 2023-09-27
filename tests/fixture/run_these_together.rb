27b6d43bbddb15783ed09fea4b396d6182d2a5a2
Justin Searls <searls@gmail.com>
2023-09-27 06:45:29 -0400

    make this example read more clearly

--- tests/fixture/run_these_together.rb
+++ tests/fixture/run_these_together.rb
@@ -1,35 +1,4 @@
 class TA < TLDR
-  # The setup hook in TA will implicate both tests, but the dependency in
-  # TC is isolated to :test_2
-  run_these_together! [
-    [TA, nil],
-    ["TC", :test_2]
-  ]
-
-  @@woah = 0
-  def self.woah
-    @@woah
-  end
-
-  def setup
-    TA.woah = 0
-  end
-
-  def self.woah= woah
-    @@woah = woah
-  end
-
-  def test_1
-    TA.woah += 1
-    sleep 0.1
-    assert_equal 1, TA.woah
-  end
-
-  def test_2
-  end
-end
-
-class TB < TLDR
   run_these_together!
 
   @@nonsense = 0
@@ -51,6 +20,37 @@ class TB < TLDR
   end
 end
 
+class TB < TLDR
+  # The setup hook in TB will implicate both tests, but TC's dependency on
+  # TB.woah is isolated to TC#test_2
+  run_these_together! [
+    [TB, nil],
+    ["TC", :test_2]
+  ]
+
+  @@woah = 0
+  def self.woah
+    @@woah
+  end
+
+  def setup
+    TB.woah = 0
+  end
+
+  def self.woah= woah
+    @@woah = woah
+  end
+
+  def test_1
+    TB.woah += 1
+    sleep 0.1
+    assert_equal 1, TB.woah
+  end
+
+  def test_2
+  end
+end
+
 class TC < TLDR
   def teardown
   end
@@ -59,9 +59,9 @@ class TC < TLDR
   end
 
   def test_2
-    TA.woah += 1
+    TB.woah += 1
     sleep 0.1
-    assert_equal 1, TA.woah
-    TA.woah = 0
+    assert_equal 1, TB.woah
+    TB.woah = 0
   end
 end
