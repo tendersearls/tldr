a51192b79868e82b3f340c7433a1eb6017c807e8
Justin Searls <searls@gmail.com>
2025-03-25 14:03:49 -0400

    try getting back into the tldr thing

--- tests/fixture/parallel.rb
+++ tests/fixture/parallel.rb
@@ -3,7 +3,7 @@ class Parallel < TLDR
   #  $ time be tldr tests/fixture/parallel.rb
   # TODO - think of a clever way to assert runtime without just making the test suite slow in the precess
   (Concurrent.processor_count * 4).times do |i|
-    define_method "test_#{i}" do
+    define_method :"test_#{i}" do
       sleep rand 0.2..0.8
       if i % 4 == 0
         assert false, "failing every fourth test and this is #{i}"
